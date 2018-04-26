7f21e63deee170ed2b4b99372f61ed6752fdfbbe
Derek Kim <bluewhale8202@gmail.com>
2018-04-26 19:24:28 +0900

    no comment

--- model.py
+++ model.py
@@ -6,23 +6,25 @@ import torch.nn.functional as F
 
 # TODO: mini-batch mode support
 
-
-# Becuase Head doensn't have any parameters,
-# it doesn't make much sense to inherit torch.nn.Module.
-
-class Head:
-    def __init__(self, N, M, shift_range=3):
+class Head(nn.Module):
+    def __init__(self, N, M, in_size, shift_range=3):
+        super().__init__()
         self.N, self.M = N, M
         self.shift_range = shift_range
-        self.in_features = self.M + self.shift_range + 3
+        self.fc = nn.Linear(in_size, self.M + self.shift_range + 3)
+        self.reset_parameters()
         self.reset_state()
+
+    def reset_parameters(self):
+        nn.init.xavier_uniform_(self.fc.weight, gain=1.4)
+        nn.init.normal_(self.fc.bias, std=0.01)
 
     def reset_state(self):
         self.w_prev = torch.zeros((1, self.N), dtype=torch.float32)
         self.history = []  # history is only used for debug purpose
 
-    # ********************** Addressing Implementation ***********************
-    # Following methods have been named after Fig.2
+    # ###################### Addressing Implementation ######################
+    # Following methods were named after Fig.2 of Graves et al.
     def content_addressing(self, mem, key, stren):
         w_tmp = F.cosine_similarity(mem, key.view(1, -1), 1, 1e-16)
         w_c = F.softmax(stren * w_tmp, dim=-1)
@@ -44,22 +46,31 @@ class Head:
         w_tmp = w_shifted ** sharp
         w = w_tmp / (torch.sum(w_tmp) + 1e-16)
         return w
-    # ************************************************************************
+    # ###
+    # #######################################################################
 
-    def __call__(self, controller_outputs, memory):
-        assert controller_outputs.shape[1] == self.in_features
+    def forward(self, controller_outputs, memory):
+        outputs = self.fc(controller_outputs)
+
         # key  : k, key vector for content-based addressing
         # stren: β, key strength scalar
         # gate : g, interpolation gate scalar
         # shift: s, shift weighting vector
         # sharp: γ, sharpening scalar
 
-        key = controller_outputs[:, :self.M]
-        other_params = controller_outputs[:, self.M:]
+        key = outputs[:, :self.M]
+        other_params = outputs[:, self.M:]
 
         stren, gate, sharp = torch.split(other_params[:, :3], (1, 1, 1), dim=1)
         shift = other_params[:, 3:]
+
         assert shift.shape[1] == self.shift_range
+
+        key = torch.tanh(key)
+        stren = F.softplus(stren)
+        gate = torch.sigmoid(gate)
+        shift = torch.softmax(shift, dim=-1)
+        sharp = 1 + F.softplus(sharp)  # TODO: check
 
         w_c = self.content_addressing(memory, key, stren)
         w_g = self.interpolation(w_c, self.w_prev, gate)
@@ -71,62 +82,57 @@ class Head:
 
 
 class Controller(nn.Module):
-    # TODO: Make hidden_features_1, hidden_features_2 configurable
-    def __init__(self, in_features, out_features,
-                 hidden_features_1=256, hidden_features_2=512):
+    def __init__(self, in_size, hidden_size, out_size):
         super().__init__()
-        # 얘네 구조는 논문 그대로는 아니라고 밝히기 ㅠ
-        self.fc1 = nn.Linear(in_features, hidden_features_1)
-        self.fc2 = nn.Linear(hidden_features_1, hidden_features_2)
-        self.fc3 = nn.Linear(hidden_features_2, out_features)
+        self.fc1 = nn.Linear(in_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, out_size)
         self.reset_parameters()
 
     def reset_parameters(self):
         nn.init.xavier_uniform_(self.fc1.weight, gain=1.4)
         nn.init.xavier_uniform_(self.fc2.weight, gain=1.4)
-        nn.init.xavier_uniform_(self.fc3.weight, gain=1.4)
         nn.init.normal_(self.fc1.bias, std=0.01)
         nn.init.normal_(self.fc2.bias, std=0.01)
-        nn.init.normal_(self.fc3.bias, std=0.01)
 
     def forward(self, x, prev_read):
         x = torch.cat((x, prev_read), dim=1)
         x = torch.sigmoid(self.fc1(x))
         x = torch.sigmoid(self.fc2(x))
-        # Skip activaiton for the final layer
-        x = torch.sigmoid(self.fc3(x))
         return x
 
 
 class NTM(nn.Module):
-    def __init__(self, N, M, in_seq_width, out_seq_width, shift_range=3):
+    def __init__(self, N, M, in_seq_width, out_seq_width,
+                 ctr_hidden_size, ctr_out_size, shift_range=3):
         super().__init__()
         self.N, self.M = N, M
         self.in_seq_width = in_seq_width
 
         # TODO: support multiple heads for read/write
-        # Assuming read_head.in_features == write_head.in_features
-        self.read_head = Head(N, M, shift_range=shift_range)
-        self.write_head = Head(N, M, shift_range=shift_range)
+        self.read_head = Head(N, M, ctr_out_size, shift_range=shift_range)
+        self.write_head = Head(N, M, ctr_out_size, shift_range=shift_range)
 
-        self.head_in = self.read_head.in_features
         self.controller = Controller(
-            self.in_seq_width + M,
-            self.head_in * 2 + M * 2)  # M * 2: erase and add in size
+            self.in_seq_width + M, ctr_hidden_size, ctr_out_size)
 
+        self.erase_add_fc = nn.Linear(256, M * 2)
         self.fc = nn.Linear(self.in_seq_width + M, out_seq_width)
+        self.reset_parameters()
+
         self.memory = torch.zeros((N, M), dtype=torch.float32)
-        # self.prev_read = torch.zeros((1, N), dtype=torch.float32)
 
     def reset_parameters(self):
+        nn.init.xavier_uniform_(self.erase_add_fc.weight, gain=1.4)
         nn.init.xavier_uniform_(self.fc.weight, gain=1.4)
+        nn.init.normal_(self.erase_add_fc.bias, std=0.5)
         nn.init.normal_(self.fc.bias, std=0.5)
 
     def reset_state(self):
         stdev = 1 / (np.sqrt(self.N + self.M))
         self.memory = nn.init.uniform(
             torch.Tensor(self.N, self.M), -stdev, stdev)
-        self.prev_read = torch.tanh(torch.randn(1, self.M, dtype=torch.float32))
+        self.prev_read = torch.tanh(
+            torch.randn(1, self.M, dtype=torch.float32))
         self.read_head.reset_state()
         self.write_head.reset_state()
 
@@ -135,9 +141,12 @@ class NTM(nn.Module):
         return w @ self.memory
 
     def write(self, controller_outputs):
-        outputs, a, e = torch.split(
-            controller_outputs, (self.head_in, self.M, self.M), dim=1)
-        w = self.write_head(outputs, self.memory)
+        w = self.write_head(controller_outputs, self.memory)
+
+        ea = self.erase_add_fc(controller_outputs)
+        e, a = ea[:, :self.M], ea[:, self.M:]
+        e = torch.sigmoid(e)
+        a = torch.tanh(a)
 
         # Assuming batch size is always 1
         w = torch.squeeze(w)
@@ -152,20 +161,16 @@ class NTM(nn.Module):
         self.memory = mem
 
     def forward(self, x=None):
-        # import ipdb
-        # ipdb.set_trace(context=9)
         if x is None:
             assert hasattr(self, 'batch_size')
             x = torch.zeros(self.batch_size, self.in_seq_width)
         self.batch_size = x.shape[0]
 
         controller_outputs = self.controller(x, self.prev_read)
-        routputs = controller_outputs[:, :self.head_in]
-        woutputs = controller_outputs[:, self.head_in:]
 
-        # Should I clone this?
-        self.prev_read = read_out = self.read(routputs)
-        self.write(woutputs)
+        # TODO: need to clone this?
+        self.prev_read = read_out = self.read(controller_outputs)
+        self.write(controller_outputs)
 
         x = torch.cat((x, read_out), 1)
         x = torch.sigmoid(self.fc(x))
